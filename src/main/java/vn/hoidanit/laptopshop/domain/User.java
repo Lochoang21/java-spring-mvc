92ddcd58aa91663142c3b95747fabb2726e39845
LocHoang1 <lochoang2101@gmail.com>
2024-08-01 15:43:20 +0700

    create file

--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -21,6 +21,7 @@ public class User {
     private String fullName;
     private String address;
     private String phone;
+    private String avatar;
 
     
 
@@ -73,12 +74,22 @@ public class User {
     public void setPhone(String phone) {
         this.phone = phone;
     }
-    
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
     @Override
     public String toString() {
         return "User [id=" + id + ", email=" + email + ", password=" + password + ", fullName=" + fullName
-                + ", address=" + address + ", phone=" + phone + "]";
+                + ", address=" + address + ", phone=" + phone + ", avatar=" + avatar + "]";
     }
+    
+    
 
     
 }
